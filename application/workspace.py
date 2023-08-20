ded65620ff61dd87157bf345e67d2ebb73314a68
dmjovan <dmjovan@gmail.com>
2023-08-21 01:22:29 +0200

    Add: Integrated all workspaces

--- application/workspace.py
+++ application/workspace.py
@@ -49,7 +49,7 @@ class Workspace(metaclass=ABCMeta):
     def render_image(self, rel_x: float, rel_y: float, horizontal_angle: int, vertical_angle: int) -> np.ndarray:
         coordinates: COORD = self._transform_relative_coordinates(rel_x, rel_y, horizontal_angle, vertical_angle)
 
-        print(f"Transformed coordinates are: {coordinates}\n"
+        print(f"Transformed coordinates are: \n{coordinates}\n"
               f"-------------------------------------------------------------")
 
         image_array = self._nerf_inference.render_coordinates(coordinates)
@@ -66,20 +66,26 @@ class OfficeTokyoWorkspace(Workspace):
                          model_path="nerf/experiments/office_tokyo/1/checkpoints/100000.ckpt")
 
         # X, Y, Z coordinates
-        self._x_max = 1.5
-        self._x_min = -1.2
+        self._x_prim_max = 1.5
+        self._x_prim_min = -1.2
 
         self._fixed_y = -0.5
 
-        self._z_max = 1.2
-        self._z_min = -0.6
+        self._z_prim_max = 1.2
+        self._z_prim_min = -0.6
+
+        # Coordinate systems difference angle
+        self._angle_diff = -10.0
 
         # Euler angles
         self._fixed_roll = 180.0
 
     def _transform_relative_coordinates(self, rel_x: float, rel_y: float, hor_angle: int, ver_angle: int) -> COORD:
-        x = (self._x_min - self._x_max) * rel_y + self._x_max
-        z = (self._z_min - self._z_max) * rel_x + self._z_max
+        x_prim = (self._x_prim_min - self._x_prim_max) * rel_y + self._x_prim_max
+        z_prim = (self._z_prim_min - self._z_prim_max) * rel_x + self._z_prim_max
+
+        x = x_prim / np.cos(self._angle_diff / 180.0 * np.pi)
+        z = z_prim / np.cos(self._angle_diff / 180.0 * np.pi)
 
         return COORD(x=x, y=self._fixed_y, z=z,
                      yaw=(-1.0) * hor_angle, pitch=(-1.0) * ver_angle, roll=self._fixed_roll)
@@ -94,14 +100,29 @@ class OfficeNewYorkWorkspace(Workspace):
                          model_path="nerf/experiments/office_new_york/1/checkpoints/200000.ckpt")
 
         # X, Y, Z coordinates
+        self._x_prim_max = 1.8
+        self._x_prim_min = -1.2
+
         self._fixed_y = -0.5
+
+        self._z_prim_max = 1.2
+        self._z_prim_min = -1.0
+
+        # Coordinate systems difference angle
+        self._angle_diff = 45.0
 
         # Euler angles
         self._fixed_roll = 180.0
 
     def _transform_relative_coordinates(self, rel_x: float, rel_y: float, hor_angle: int, ver_angle: int) -> COORD:
-        return COORD(x=rel_x, y=self._fixed_y, z=rel_y,
-                     yaw=float(hor_angle), pitch=float(ver_angle), roll=self._fixed_roll)
+        x_prim = (self._x_prim_min - self._x_prim_max) * rel_x + self._x_prim_max
+        z_prim = (self._z_prim_min - self._z_prim_max) * rel_y + self._z_prim_max
+
+        x = x_prim / np.cos(self._angle_diff / 180.0 * np.pi)
+        z = z_prim / np.cos(self._angle_diff / 180.0 * np.pi)
+
+        return COORD(x=x, y=self._fixed_y, z=z,
+                     yaw=(-1.0) * hor_angle, pitch=(-1.0) * ver_angle, roll=self._fixed_roll)
 
 
 class OfficeGeneveWorkspace(Workspace):
@@ -113,14 +134,29 @@ class OfficeGeneveWorkspace(Workspace):
                          model_path="nerf/experiments/office_geneve/1/checkpoints/200000.ckpt")
 
         # X, Y, Z coordinates
+        self._x_prim_max = 1.0
+        self._x_prim_min = -1.8
+
         self._fixed_y = -0.5
+
+        self._z_prim_max = 3.3
+        self._z_prim_min = -1.4
+
+        # Coordinate systems difference angle
+        self._angle_diff = 20.0
 
         # Euler angles
         self._fixed_roll = 180.0
 
     def _transform_relative_coordinates(self, rel_x: float, rel_y: float, hor_angle: int, ver_angle: int) -> COORD:
-        return COORD(x=rel_x, y=self._fixed_y, z=rel_y,
-                     yaw=float(hor_angle), pitch=float(ver_angle), roll=self._fixed_roll)
+        x_prim = (self._x_prim_min - self._x_prim_max) * rel_y + self._x_prim_max
+        z_prim = (self._z_prim_min - self._z_prim_max) * rel_x + self._z_prim_max
+
+        x = x_prim / np.cos(self._angle_diff / 180.0 * np.pi)
+        z = z_prim / np.cos(self._angle_diff / 180.0 * np.pi)
+
+        return COORD(x=x, y=self._fixed_y, z=z,
+                     yaw=(-1.0) * hor_angle, pitch=(-1.0) * ver_angle, roll=self._fixed_roll)
 
 
 class OfficeBelgradeWorkspace(Workspace):
@@ -132,11 +168,26 @@ class OfficeBelgradeWorkspace(Workspace):
                          model_path="nerf/experiments/office_belgrade/1/checkpoints/200000.ckpt")
 
         # X, Y, Z coordinates
+        self._x_prim_max = 3.0
+        self._x_prim_min = -0.5
+
         self._fixed_y = -0.5
+
+        self._z_prim_max = 3.0
+        self._z_prim_min = -1.0
+
+        # Coordinate systems difference angle
+        self._angle_diff = -10.0
 
         # Euler angles
         self._fixed_roll = 180.0
 
     def _transform_relative_coordinates(self, rel_x: float, rel_y: float, hor_angle: int, ver_angle: int) -> COORD:
-        return COORD(x=rel_x, y=self._fixed_y, z=rel_y,
-                     yaw=float(hor_angle), pitch=float(ver_angle), roll=self._fixed_roll)
+        x_prim = (self._x_prim_min - self._x_prim_max) * rel_y + self._x_prim_max
+        z_prim = (self._z_prim_min - self._z_prim_max) * rel_x + self._z_prim_max
+
+        x = x_prim / np.cos(self._angle_diff / 180.0 * np.pi)
+        z = z_prim / np.cos(self._angle_diff / 180.0 * np.pi)
+
+        return COORD(x=x, y=self._fixed_y, z=z,
+                     yaw=(-1.0) * hor_angle, pitch=(-1.0) * ver_angle, roll=self._fixed_roll)
